b747036e12ef73e0b545036e59a26babd30c9a29
Lucas Lança <63751427+LucasLanca@users.noreply.github.com>
2021-08-06 22:19:08 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_tudo_gostoso/screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
---
+++ lib/style.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/painting.dart';
+
+// Laranja
+Color orangeTheme = Color(0xFFFF3D0A);
